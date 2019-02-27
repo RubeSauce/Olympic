d782e4b4d81e554c12729733736685de4582d089
Ruben Gonzalez <43782056+RubeSauce@users.noreply.github.com>
2019-02-27 09:01:43 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,75 @@
 const express = require('express');
 const app = express();
+let students = [];
+
+fs.readFile('students.json',(err, data) => {
+    students = JSON.parse(data);
+})
+
+
+/*if ( $("#username").val()== "" ||  $("#lastname").val() == "" ||  $("#grade").val() == "" {
+                 alert("Please Fill out the Fields");
+                       
+              } else {
+                 // check make sure it checks for the data in the array
+                    First Name = $("#firstname").val();
+                   Last Name = $("#lastname").val();
+                  Grade = $("#grade").val();
+                  
+                  
+                 students =  JSON.parse(localStorage.getItem("students"))
+                  
+                  students.push({
+                     FirstName: FirstName,
+                     LastName: LastName,
+                     Grade: Grade,
+                     
+                  });
+                 
+             })
+       ;   
+     */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ fs.writeFile('students.json',JSON.stringify(students),(err) => {
+        console.log(students)
+        console.warn(err);
+        })
+
+
+
+
+
 
 app.use(express.static('public'));
 app.use(express.json());
